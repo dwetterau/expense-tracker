9f32930d7d8191140b8184b538e5289cb1fb17b3
Adam Faulkner <adamfaulkner1@gmail.com>
2013-08-20 22:39:07 -0500

    Switched completely to node-cassandra-cql

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 var express = require('express');
-var helenus = require('helenus');
+//var helenus = require('helenus');
+var Client = require('node-cassandra-cql').Client;
 var Q = require('q');
 var uuid = require('node-uuid');
 
 var app = express();
-var pool = new helenus.ConnectionPool({
-    hosts: ['localhost:9160'],
-    keyspace: 'expense_tracker',
-    cqlVersion : '3.0.0'
+var cql_client = new Client({hosts: ['localhost:9042'],
+                             keyspace: 'expense_tracker',
+                             version: '3.0.0',
+                             getAConnectionTimeout: 1000});
+
+cql_client.on('log', function(level, message) {
+    console.log('log event %s - %j', level, message);
 });
 
-pool.on('error', function(err) {
-    console.error(err.name, err.message);
+cql_client.on('error', function(error) {
+    console.error(error);
 });
-
-// Connect to the database
-var connection_done = Q.nfcall(pool.connect.bind(pool))
-    .fail(function(err) {
-        console.error('Error connecting to db', err);
-    });
 
 function execute_cql() {
     var cql_args = arguments;
     console.log('args: ', cql_args);
-    return connection_done.then(function() {
-        console.log('Executing');
-        return Q.npost(pool, 'cql', cql_args);
-    });
+    console.log('Executing');
+    return Q.npost(cql_client, 'execute', cql_args);
 }
 
 function create_image_table() {
@@ -40,4 +36,30 @@ function create_image_table() {
                 });
 }
 
+function store_image(image_data, metadata, thumbnails) {
+    execute_cql('INSERT INTO images (image_id, image_data, metadata, thumbnails)' +
+                ' VALUES (now(), ?, ?, ?)', [image_data, metadata, thumbnails]).then(
+                    function(retval) {
+                        console.log('done!', retval);
+                    }, function(err) {
+                        console.error('error: ', err);
+                    });
+}
+
+function testing() {
+    execute_cql('SELECT * FROM images').then(function(result) {
+        console.log('result ', result);
+        console.log('going');
+        console.log('metadata', result.rows[0].get('metadata'));
+        console.log('done');
+    }, function(err) {
+        console.log('there was an error!', err);
+    }).fail(function(err) {
+        console.log('There was an error printing', err);
+    });
+}
+
 exports.create_image_table = create_image_table;
+exports.store_image = store_image;
+exports.testing = testing;
+exports.execute = cql_client.execute.bind(cql_client);
