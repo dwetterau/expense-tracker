b5ba3b99f9ad37b70e6d26004330e084649f7a47
Adam Faulkner <adamfaulkner1@gmail.com>
2013-08-21 01:45:44 -0500

    Basic pass at storing images with metadata complete

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-var express = require('express');
-//var helenus = require('helenus');
 var Client = require('node-cassandra-cql').Client;
 var Q = require('q');
-var uuid = require('node-uuid');
-
-var app = express();
+//var express = require('express');
+//var uuid = require('node-uuid');
+//var app = express();
 var cql_client = new Client({hosts: ['localhost:9042'],
                              keyspace: 'expense_tracker',
                              version: '3.0.0',
                              getAConnectionTimeout: 1000});
 var ExifImage = require('exif').ExifImage;
-
-/*cql_client.on('log', function(level, message) {
-    console.log('log event %s - %j', level, message);
-});
-
-cql_client.on('error', function(error) {
-    console.error(error);
-});*/
-
 var execute_cql = Q.nbind(cql_client.execute, cql_client);
 
 function extract_metadata(image_data) {
-    // This api is really stupid, replace this with something more sane
+    // This library seems really stupid, replace this with something more sane
     var deferred = Q.defer();
     try {
         new ExifImage({image: image_data}, function(err, data) {
@@ -51,15 +40,22 @@ function create_image_table() {
 
 function store_image(image_data, thumbnails) {
     return extract_metadata(image_data).then(function(full_metadata) {
+        console.log('full metadata: ', full_metadata);
         var location = '';
         if (full_metadata.gps) {
             var full_latitude = full_metadata.gps.GPSLatitude;
-            var latitude = full_latitude[0] + full_latitude[1] / 60 + full_latitude[2] / 3600;
-            var full_longitude = full_metadata.gps.GPSLongitude;
-            var longitude = full_longitude[0] + full_longitude[1] / 60 + full_longitude[2] / 3600;
-            location = latitude + ',' + longitude;
+            if (full_latitude) {
+                var latitude = full_latitude[0] + full_latitude[1] / 60 + full_latitude[2] / 3600;
+                var full_longitude = full_metadata.gps.GPSLongitude;
+                var longitude = full_longitude[0] + full_longitude[1] / 60 + full_longitude[2] / 3600;
+                location = latitude + ',' + longitude;
+            }
         }
-        return {'location': location};
+        var date = '';
+        if (full_metadata.image && full_metadata.image.ModifyDate) {
+            date = full_metadata.image.ModifyDate;
+        }
+        return {'location': location, 'date': date};
     }, function(err) {
         console.error('Metadata extraction failed', err);
         return {};
@@ -72,24 +68,10 @@ function store_image(image_data, thumbnails) {
         function(retval) {
             console.log('done!', retval);
         }, function(err) {
-            console.error('error: ', err);
+            console.error('Error completing store: ', err);
         });
-}
-
-function testing() {
-    execute_cql('SELECT * FROM images').then(function(result) {
-        console.log('result ', result);
-        console.log('going');
-        console.log('metadata', result.rows[0].get('metadata'));
-        console.log('done');
-    }, function(err) {
-        console.log('there was an error!', err);
-    }).fail(function(err) {
-        console.log('There was an error printing', err);
-    });
 }
 
 exports.create_image_table = create_image_table;
 exports.store_image = store_image;
-exports.testing = testing;
 exports.execute = cql_client.execute.bind(cql_client);
