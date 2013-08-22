46dd9f3f954a4b3f327f63a9525df76c41e5f7cf
dwetterau <david.wetterau@gmail.com>
2013-08-21 21:15:36 -0700

    started user creation, not tested with cassandra yet

--- main.js
+++ main.js
@@ -2,8 +2,46 @@ var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 var images = require('./images');
+var users = require('./users');
 var Q = require('q');
 var fs = require('fs');
+
+// User routes
+
+app.get('/user/:id', function(req, res) {
+  var user_id = req.params.id;
+  users.get_user(user_id).then(function(data) {
+    res.send('Retrieved user data: ' + data);
+  }, function(err) {
+    res.send(500, 'An error occured making the account: ' + err);
+  });
+});
+
+app.post('/make_account', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var new_user = {
+    email: email,
+    password: password
+  }
+  users.make_user(new_user).then(function(user_id) {
+    res.redirect('/user/' + user_id);
+  }, function(err) {
+    res.send(500, 'An error occured making the account: ' + err);
+  });
+});
+
+app.get('/make_account', function(req, res) {
+  res.send('<html><body><form method="post" enctype="multipart/form-data">' +
+           '<div><label for="email">Email: </label>' +
+           '<input type="email" name="email" required="required" /></div>' +
+           '<div><label for="password">Password: </label>' +
+           '<input type="password" name="password" required="required" /></div>' +
+           '<input type="submit" value="Create Account" />' +
+           '</form></body> </html>');
+});
+
+// Image routes
 
 app.get('/images/:uuid', function(req, res) {
   var image_id = req.params.uuid;
