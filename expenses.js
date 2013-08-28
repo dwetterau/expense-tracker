ce387d3e16f6aa0910743ff6e7c789ea94c1d0d5
Adam Faulkner <adamfaulkner1@gmail.com>
2013-08-28 13:35:48 -0500

    Created status table, added updated status

--- expenses.js
+++ expenses.js
@@ -3,37 +3,53 @@ var users = require('./users');
 var Q = require('q');
 var uuid = require('node-uuid');
 
+// constants for expense state
+var expense_states = {
+  WAITING: 0,
+  PAID: 1,
+};
+
 function create_expense_tables() {
-  return execute_cql('CREATE TABLE expenses (' +
-                     'expense_id uuid PRIMARY KEY, ' +
-                     'title text, ' +
-                     'description text, ' +
-                     'value int, ' +
-                     'participants set<uuid>, ' +
-                     'receipt_image uuid)');
+  var create_expenses = execute_cql(
+    'CREATE TABLE expenses (' +
+      'expense_id uuid PRIMARY KEY, ' +
+      'title text, ' +
+      'description text, ' +
+      'value int, ' +
+      'participants map<uuid, int>, ' +
+      'receipt_image uuid)'
+  );
+  var create_status = execute_cql(
+    'CREATE TABLE expense_status (' +
+      'user_id uuid, ' +
+      'expense_id uuid, ' +
+      'status int, ' +
+      'PRIMARY KEY (user_id, expense_id))'
+  );
+  return Q.all([create_expenses, create_status]);
 }
 
 function store_expense(expense) {
   var id = uuid.v4();
-  console.log(expense.participants);
+  var user_ids = [];
   return Q.all(
     // Convert emails to uuids
     expense.participants.map(function(email) {
-      console.log(email);
       return users.get_by_email(email).then(function(result) {
         return result.id;
       });
     })
-  ).then(function(user_ids) {
-    // TODO: make a better way around this
-    var user_id_cql = {value: user_ids, hint: 'set'};
-    console.log(user_id_cql);
-    console.log('executing');
-    console.log('user_ids: ', user_id_cql);
+  ).then(function(retreived_ids) {
+    user_ids = retreived_ids;
+  }).then(function() {
+    var users_status = {};
+    user_ids.forEach(function(user_id) {
+      users_status[user_id] = expense_states.WAITING;
+    });
     return execute_cql('INSERT INTO expenses ' +
                        '(expense_id, title, value, participants) ' +
                        'VALUES (?, ?, ?, ?)',
-                       [id, expense.title, parseInt(expense.value, 10), user_id_cql]);
+                       [id, expense.title, parseInt(expense.value, 10), users_status]);
   }).then(function() {
     // TODO: abstract this out
     // this is messy
@@ -44,6 +60,13 @@ function store_expense(expense) {
                          [expense.description, id]
                         );
     }
+  }).then(function() {
+    return user_ids.map(function(user_id) {
+      return execute_cql('INSERT INTO expense_status ' +
+                         '(user_id, expense_id, status)' +
+                         ' VALUES (?, ?, ?)',
+                         [user_id, id, 0]);
+    });
   }).then(function() {
     return id;
   });
@@ -59,7 +82,11 @@ function get_expense(id) {
       template_data.title = result.rows[0].get('title');
       template_data.description = result.rows[0].get('description');
       template_data.receipt_image = result.rows[0].get('receipt_image');
-      var participant_uuids = result.rows[0].get('participants');
+      var participants_status = result.rows[0].get('participants');
+      var participant_uuids = [];
+      for (var uuid in participants_status) {
+        participant_uuids.push(uuid);
+      }
       template_data.value = result.rows[0].get('value');
       return Q.all(
         participant_uuids.map(
