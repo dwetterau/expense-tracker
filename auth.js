ab443a32d97da3be5c6141a5299fc64b974a1809
dwetterau <david.wetterau@gmail.com>
2013-08-28 08:32:00 -0700

    added auth file

---
+++ auth.js
@@ -0,0 +1,37 @@
+var crypto = require('crypto');
+
+function is_logged_in(req) {
+  return req.session.user_id !== undefined;
+}
+
+function check_auth(req, res, next) {
+  if (!is_logged_in(req)) {
+    throw new Error("Not logged in");
+  } else {
+    get_user(req.session.user_id).then(function(user) {
+      req.user = user;
+      next();
+    });
+  }
+}
+
+function generate_salt(len) {                                                
+  return new Buffer(crypto.randomBytes(len)).toString('base64');          
+}                                                                           
+                                                                            
+function hash_password(password, salt) {                          
+  var deferred = Q.defer();
+  crypto.pbkdf2(password, salt, 10000, 512, function(err, dk) {           
+    if (err) {                                                          
+      console.error('Error hashing a password');                      
+      throw new Error(err);                                           
+    } else {                                                            
+      deferred.resolve(new Buffer(dk).toString('base64'));                    
+    }                                                                   
+  });                                                                     
+  return deferred.promise;                                                                 
+}
+
+exports.check_auth = check_auth;
+exports.generate_salt = generate_salt;
+exports.hash_password = hash_password;
