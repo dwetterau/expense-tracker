f6f9f107467e72d579261a7b61958ad8414a3e9f
Adam Faulkner <adamfaulkner1@gmail.com>
2014-03-11 00:53:36 -0500

    Fixed some build things, firefox source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,40 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
+/* Currently, there's some issue with Firefox's sourceMaps
+   that makes it so that they don't work when using absolute
+   urls. Use this module to eliminate the urls
+*/
+var path = require('path');
+var mold = require('mold-source-map');
+var root = path.join(__dirname, 'ui');
+
+gulp.task('watch-scripts', function() {
+  var bundler = watchify('./ui/ui_main.js');
+
+  function rebundle() {
+    bundler.bundle({debug: true})
+    .pipe(mold.transformSourcesRelativeTo(root))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./ui'));
+  }
+  bundler.on('update', rebundle);
+  return rebundle();
+});
+
+gulp.task('nodemon', function() {
+  return nodemon({script: 'main.js'});
 });
 
 gulp.task('scripts', function() {
-  // Only run the entrypoint, browserify gets the dependencies
-  return gulp.src('ui/ui_main.js')
-    .pipe(browserify({
-      debug: true
-    }))
-    .pipe(gulp.dest('./build'));
+  var bundler = browserify('./ui/ui_main.js');
+  return bundler.bundle({debug: true})
+    .pipe(mold.transformSourcesRelativeTo(root))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./ui'));
 });
+
+gulp.task('dev', ['nodemon', 'watch-scripts']);
